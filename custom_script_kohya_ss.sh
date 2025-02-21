c209eeb98f8fe0c68c33ff10b2cca569c3745177
VjumpKunG <vjumpkung@gmail.com>
2025-02-21 12:43:21 +0700

    update

--- custom_script_kohya_ss.sh
+++ custom_script_kohya_ss.sh
@@ -2,6 +2,10 @@
 
 echo "Updating kohya-ss GUI"
 
+curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/kohya_ss_notebooks/run_kohya_ss_gui.ipynb >run_kohya_ss_gui.ipynb
+curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/kohya_ss_notebooks/ui/main.py >./ui/main.py
+curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/kohya_ss_notebooks/ui/google_drive_download.py >./ui/google_drive_download.py
+
 cd kohya_ss && git pull && python ./setup/validate_requirements.py
 
 echo "Update Completed"
