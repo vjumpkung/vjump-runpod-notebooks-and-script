dcebf69bdad7bae51d461a460a87c81e01173469
VjumpKunG <vjumpkung@gmail.com>
2025-02-21 13:24:33 +0700

    update custom script of kohya_ss

--- custom_script_kohya_ss.sh
+++ custom_script_kohya_ss.sh
@@ -6,6 +6,6 @@ curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-scri
 curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/kohya_ss_notebooks/ui/main.py >./ui/main.py
 curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/kohya_ss_notebooks/ui/google_drive_download.py >./ui/google_drive_download.py
 
-cd kohya_ss && git pull && python ./setup/validate_requirements.py
+cd kohya_ss && git pull && python ./setup/validate_requirements.py --requirements requirements_runpod.txt
 
 echo "Update Completed"
