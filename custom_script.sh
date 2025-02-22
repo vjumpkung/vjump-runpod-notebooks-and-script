4710231d8732cc29b0e19f9f5d65da4be675dabb
vjumpkung <vjumpkung@gmail.com>
2025-02-22 17:28:40 +0700

    add facerestorer model

--- custom_script.sh
+++ custom_script.sh
@@ -10,7 +10,29 @@ update_comfyui() {
     echo "Update ComfyUI Completed"
 }
 
+download_facerestore_model() {
+    # Set the target directory
+    TARGET_DIR="./ComfyUI/models/facerestore_models/"
+
+    # Create the directory if it doesn't exist
+    mkdir -p "$TARGET_DIR"
+
+    # List of model URLs
+    urls=(
+        "https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/facerestore_models/GFPGANv1.3.pth"
+        "https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/facerestore_models/GFPGANv1.4.pth"
+        "https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/facerestore_models/codeformer-v0.1.0.pth"
+        "https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/facerestore_models/GPEN-BFR-512.onnx"
+    )
+
+    # Download each file using wget
+    for url in "${urls[@]}"; do
+        wget -c "$url" -P "$TARGET_DIR"
+    done
+}
+
 curl https://raw.githubusercontent.com/vjumpkung/vjump-comfyui-runpod-template/refs/heads/main/src/extra_model_paths.yaml >/notebooks/ComfyUI/extra_model_paths.yaml
 
 make_directory
 update_comfyui
+download_facerestore_model
