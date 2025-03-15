2c837a27be6a7a6d835c1f4025db5313771b0adf
VjumpKunG <vjumpkung@gmail.com>
2025-03-16 01:31:43 +0700

    update forge

--- webui-forge/custom_scripts_forge.sh
+++ webui-forge/custom_scripts_forge.sh
@@ -2,4 +2,10 @@
 
 echo "Updating WebUI Forge GUI"
 
+cd stable-diffusion-webui-forge && git pull --ff-only
+
+curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/webui-forge/run_kohya_ss_gui.ipynb >run_kohya_ss_gui.ipynb
+curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/webui-forge/ui/main.py >./ui/main.py
+curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/webui-forge/ui/google_drive_download.py >./ui/google_drive_download.py
+
 echo "Update Completed"
