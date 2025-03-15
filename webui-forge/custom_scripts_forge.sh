3d999f65d221fddfb71cebde9f56a0241952d195
VjumpKunG <vjumpkung@gmail.com>
2025-03-16 01:40:04 +0700

    update script???

--- webui-forge/custom_scripts_forge.sh
+++ webui-forge/custom_scripts_forge.sh
@@ -4,6 +4,7 @@ echo "Updating WebUI Forge GUI"
 
 cd stable-diffusion-webui-forge && git pull --ff-only
 
+cd /notebooks/
 curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/webui-forge/launch_webui_forge.ipynb >launch_webui_forge.ipynb
 curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/webui-forge/resource_manager.ipynb >resource_manager.ipynb
 curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/webui-forge/ui/main.py >./ui/main.py
