9cd0e45cb4784a4b390d5c71ca300d311cb87f1d
VjumpKunG <vjumpkung@gmail.com>
2025-05-15 22:30:46 +0700

    update page logo

--- webui-forge/custom_scripts_forge.sh
+++ webui-forge/custom_scripts_forge.sh
@@ -4,6 +4,9 @@ echo "Updating WebUI Forge GUI"
 
 cd stable-diffusion-webui-forge && git pull --ff-only
 
+export TORCH_FORCE_WEIGHTS_ONLY_LOAD=1
+pip install pydantic==2.8.0
+
 cd /notebooks/
 curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/webui-forge/launch_webui_forge.ipynb >launch_webui_forge.ipynb
 curl https://raw.githubusercontent.com/vjumpkung/vjump-runpod-notebooks-and-script/refs/heads/main/webui-forge/resource_manager.ipynb >resource_manager.ipynb
